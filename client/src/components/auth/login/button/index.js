901a0b03f7b49c7083ab51e8f1e00dd1e858e584
jimbomaniak <jimboman54321@gmail.com>
2018-08-02 19:41:30 +0300

    add Button component

---
+++ client/src/components/auth/login/button/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './button.css'
+
+const Button = (props) => (
+  <button className={`auth__button ${props.buttonType}`} onSubmit={props.onSubmit}>
+    {props.buttonText}
+  </button>
+)
+
+Button.propTypes = {
+  buttonType: PropTypes.string,
+  onSubmit: PropTypes.func,
+  buttonText: PropTypes.buttonText
+}
+
+export default Button
