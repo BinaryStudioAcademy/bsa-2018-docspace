163ac6436d8947f102b7c18c736f9e4ba655329d
jimbomaniak <jimboman54321@gmail.com>
2018-08-02 21:15:40 +0300

    add proptypes for actions, add connect to redux

--- client/src/components/auth/login/index.js
+++ client/src/components/auth/login/index.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import Input from './input/index'
 import Password from './input/password/index'
 import Button from './button/index'
+import { login } from './logic/loginActions'
 
 import './login.css'
 
-export default class Login extends Component {
+class Login extends Component {
   render () {
+    const { login } = this.props.actions
     return (
       <form className='auth_login'>
         <Button buttonType='google' buttonText='Continue with Google' />
         <Input inputType='email' label='Enter email adress' />
         <Input inputType='fullName' label='Enter full name' />
         <Password label='Create password' />
-        <Button buttonType='signUp' buttonText='Sign up' />
+        <Button buttonType='signUp' buttonText='Sign up' onSubmit={login} />
       </form>
     )
   }
 }
+
+Login.propTypes = {
+  actions: PropTypes.object
+}
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(login, dispatch)
+})
+
+export default connect(mapDispatchToProps)(Login)
