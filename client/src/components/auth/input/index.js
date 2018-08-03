46228bc255c8a34e025e4d3aa71dbf632ac88ab3
jimbomaniak <jimboman54321@gmail.com>
2018-08-03 20:22:27 +0300

    add attr to Input component

--- client/src/components/auth/input/index.js
+++ client/src/components/auth/input/index.js
@@ -4,12 +4,26 @@ import PropTypes from 'prop-types'
 import './input.css'
 
 const Input = (props) => (
-  <input type={props.inputType} placeholder={props.label} className='auth__input' />
+  <input
+    type={props.inputType}
+    placeholder={props.label}
+    className={`auth__input ${props.name}`}
+    onChange={props.onChange}
+    autoComplete={props.autoComplete}
+    name={props.name}
+    value={props.value}
+    disabled={props.disabled}
+  />
 )
 
 Input.propTypes = {
   inputType: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func,
+  autoComplete: PropTypes.bool,
+  disabled: PropTypes.bool,
+  name: PropTypes.string,
+  value: PropTypes.string
 }
 
 export default Input
