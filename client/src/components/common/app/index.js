d6d774109d071b7bd2f9ce9b86cdf7680de166a1
Danil Davydov <Linadakill@gmail.com>
2018-08-03 17:05:50 +0300

    stub for spaceModal

--- client/src/components/common/app/index.js
+++ client/src/components/common/app/index.js
@@ -1,75 +1,10 @@
 import React, { Component } from 'react'
 import logo from '../../../resources/logo.svg'
 import Greeting from '../../greeting'
-
+import SpaceModal from '../../modals/spaceModal'
 import './app.css'
 
-import Modal from '../../modal'
-import TemplateList from '../../templateList'
-
-const items = [
-  {
-    name: 'Empty space',
-    description: 'Start from empty list',
-    img: logo
-  },
-  {
-    name: 'Empty space',
-    description: 'Start from empty list',
-    img: logo
-  },
-  {
-    name: 'Empty space',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    img: logo
-  },
-  {
-    name: 'Empty space',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum suscipit ',
-    img: logo
-  },
-  {
-    name: 'Empty space',
-    description: 'Start from empty list',
-    img: logo
-  },
-  {
-    name: 'Empty space',
-    description: 'Start from empty list',
-    img: logo
-  },
-  {
-    name: 'Empty space',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum suscipit ',
-    img: logo
-  },
-  {
-    name: 'Empty space',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum suscipit ',
-    img: logo
-  },
-  {
-    name: 'Empty space',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum suscipit ',
-    img: logo
-  }
-
-]
-
 class App extends Component {
-  constructor () {
-    super()
-    this.state = {
-      showModal: true
-    }
-  }
-
-  toggleModal = () => {
-    this.setState({
-      showModal: !this.state.showModal
-    })
-  }
-
   render () {
     return (
       <div className='app__root'>
@@ -77,17 +12,8 @@ class App extends Component {
           <img src={logo} className={'app__logo'} alt='logo' />
           <h1 className='app__title'>Binary docspace</h1>
         </header>
-
-        <Modal
-          show={this.state.showModal}
-          toggleModal={this.toggleModal}
-        >
-          <TemplateList
-            items={items}
-          />
-        </Modal>
-        <button onClick={this.toggleModal}>Open Modal</button>
         <Greeting />
+        <SpaceModal />
       </div>
     )
   }
--- client/src/components/templateList/index.js
+++ client/src/components/templateList/index.js
@@ -2,14 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './templateList.css'
 
-const TemplateList = (props) => {
-  const items = props.items
+class TemplateList extends React.Component {
+  
+  render(){
+    console.log('render')
+      const items = this.props.items
   return (
     <ul className='template-list'>
       {
         items.map((item, index) => {
+          const isSelected = this.props.selectedItem === item
           return (
-            <li className='template' key={index}>
+            <li 
+              className={`template${isSelected ? ' selected' : ''}`} 
+              key={index}
+              onClick={() => this.props.handleSelectItem(item)}
+            >
               <img className='template-preview' src={item.img} />
               <div className='template-meta'>
                 <div className='template-name'> {item.name} </div>
@@ -21,6 +29,8 @@ const TemplateList = (props) => {
       }
     </ul>
   )
+  }
+
 }
 
 TemplateList.propTypes = {
@@ -28,7 +38,9 @@ TemplateList.propTypes = {
     name: PropTypes.string,
     description: PropTypes.string,
     img: PropTypes.string
-  }))
+  })),
+
+  selectedItem: PropTypes.object
 }
 
 export default TemplateList
