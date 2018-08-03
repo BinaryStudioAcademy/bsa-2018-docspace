2c756c06796d0776d865d4d8994fd1e50feccb7e
jimbomaniak <jimboman54321@gmail.com>
2018-08-03 16:24:53 +0300

    remove unused component

--- client/src/components/common/app/index.js
+++ client/src/components/common/app/index.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
-import Auth from '../../auth/index'
 
 import './app.css'
 
 class App extends Component {
   render () {
     return (
-      <div className='app__root'>
-        <Auth />
-      </div>
+      <div className='app__root' />
     )
   }
 }
