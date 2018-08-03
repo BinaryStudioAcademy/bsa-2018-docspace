62b0b183347258280917b7e32a4cc8d228d4ebcf
jimbomaniak <jimboman54321@gmail.com>
2018-08-03 16:24:34 +0300

    add redirect to login

--- client/src/components/auth/login/index.js
+++ client/src/components/auth/login/index.js
@@ -1,25 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
+import { Redirect } from 'react-router-dom'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import Input from './input/index'
-import Password from './input/password/index'
-import Button from './button/index'
+import Input from '../input/index'
+import Password from '../input/password/index'
+import Button from '../button/index'
 import { login } from './logic/loginActions'
 
 import './login.css'
 
 class Login extends Component {
+  state = {
+    redirectToSignup: false
+  }
+
+  handleRedirectToSignUp = () => {
+    this.setState({
+      redirectToSignup: !this.state.redirectToSignup
+    })
+  }
+
   render () {
     const { login } = this.props.actions
+    const { redirectToSignup } = this.state
+
+    if (redirectToSignup) {
+      return <Redirect to='/signup' />
+    }
     return (
-      <form className='auth_login'>
-        <Button buttonType='google' buttonText='Continue with Google' />
-        <Input inputType='email' label='Enter email adress' />
-        <Input inputType='fullName' label='Enter full name' />
-        <Password label='Create password' />
-        <Button buttonType='signUp' buttonText='Sign up' onSubmit={login} />
-      </form>
+      <Fragment>
+        <div className='auth_main'>
+          <div className='auth_content'>
+            <div className='auth__header'>
+              <img className='header__logo' src='' alt='logo' />
+              <h2>Log in to your account</h2>
+            </div>
+            <form className='auth__login'>
+              <Input inputType='email' label='Enter email adress' />
+              <Password label='Create password' />
+              <Button buttonType='login' disabled buttonText='Login' onSubmit={login} />
+              <p className='auth__footer' onClick={this.handleRedirectToSignUp}>Sign up for account</p>
+            </form>
+          </div>
+        </div>
+      </Fragment>
     )
   }
 }
