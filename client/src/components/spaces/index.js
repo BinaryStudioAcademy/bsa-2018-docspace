0e1484344c99473ca3b0ea2d7c5757e33226e8c2
Daryna Gavrylenko <deakozyulka@gmail.com>
2018-08-03 15:43:32 +0300

    feature/space-page: add global variables, create space component directory

---
+++ client/src/components/spaces/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import './space.css'
+
+class Space extends Component {
+    render() {
+        return (
+            <div>Hello from Space</div>
+        )
+    }
+}
+
+export default Space
