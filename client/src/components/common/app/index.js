86f8aec65f625fbd2df0f263e6759b7c21dd4889
Khrystyna Roboretska <christina.roboretska@gmail.com>
2018-08-06 16:36:54 +0300

    Added router

--- client/src/components/common/app/index.js
+++ client/src/components/common/app/index.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react'
-import logo from '../../../resources/logo.svg'
-import Greeting from '../../greeting'
-
+import React, {Component} from 'react'
+import Dashboard from '../../dashboard'
+import history from '../../../commonLogic/history'
+import {ConnectedRouter} from 'connected-react-router'
+import {Provider} from 'react-redux'
+import {store} from '../../../commonLogic/store'
 import './app.css'
 
 class App extends Component {
   render () {
     return (
       <div className='app__root'>
-        <header className='app__header'>
-          <img src={logo} className={'app__logo'} alt='logo' />
-          <h1 className='app__title'>Binary docspace</h1>
-        </header>
-        <Greeting />
+        <Provider store={store}>
+          <ConnectedRouter history={history}>
+            <Dashboard />
+          </ConnectedRouter>
+        </Provider>
       </div>
     )
   }
