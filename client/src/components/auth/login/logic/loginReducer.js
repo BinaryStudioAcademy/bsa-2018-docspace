6736dbff1d8edaa1a91c9ca823176eb447c7e695
jimbomaniak <jimboman54321@gmail.com>
2018-08-06 08:56:41 +0300

    add all login actions to reducer

--- client/src/components/auth/login/logic/loginReducer.js
+++ client/src/components/auth/login/logic/loginReducer.js
@@ -1,21 +1,42 @@
 import * as actionTypes from './loginActionTypes'
 
 const initialState = {
-  email: '',
-  fullName: '',
-  password: ''
+  requesting: false,
+  successful: false,
+  messages: [],
+  errors: []
 }
 
 export const loginReducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.LOGIN: {
+    case actionTypes.LOGIN_REQUESTING:
       return {
-        ...state,
-        email: action.payload.email,
-        fullName: action.payload.fullName,
-        password: action.payload.password
+        requesting: true,
+        successful: false,
+        messages: [{ body: 'Logging in...', time: new Date() }],
+        errors: []
       }
-    }
-    default: return state
+
+    case actionTypes.LOGIN_SUCCESS:
+      return {
+        errors: [],
+        messages: [action.response],
+        requesting: false,
+        successful: true
+      }
+
+    case actionTypes.LOGIN_ERROR:
+      return {
+        errors: state.errors.concat([{
+          body: action.error.toString(),
+          time: new Date()
+        }]),
+        messages: [],
+        requesting: false,
+        successful: false
+      }
+
+    default:
+      return state
   }
 }
