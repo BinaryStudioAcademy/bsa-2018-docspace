11aa8984da03f9977811983bad0c06ae1248e9a9
jimbomaniak <jimboman54321@gmail.com>
2018-08-06 08:54:20 +0300

    add login watcher

--- client/src/commonLogic/rootSaga.js
+++ client/src/commonLogic/rootSaga.js
@@ -1,11 +1,14 @@
 // import { fork } from 'redux-saga/effects'
 import signupWatcher from '../components/auth/signup/logic/signupSaga'
+import loginWatcher from '../components/auth/login/logic/loginSaga'
 // import greetingSaga from '../components/greeting/logic/greetingSaga'
 
 function * rootSaga () {
   yield [
     // fork(greetingSaga)
-    signupWatcher()
+    signupWatcher(),
+    loginWatcher()
+
   ]
 }
 
