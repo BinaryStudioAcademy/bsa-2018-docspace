fbc60ea7b330cf1a9b7aaeceaf3d2d70fa281951
jimbomaniak <jimboman54321@gmail.com>
2018-08-06 08:53:59 +0300

    add login with passport localStrategy

---
+++ server/config/passport.js
@@ -0,0 +1,36 @@
+const userRep = require('../repositories/userRepository')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+
+module.exports = () => {
+  console.log('PASSPORT EXECUTED')
+  passport.serializeUser(function (user, done) {
+    console.log('SERIAL')
+    done(null, user.id)
+  })
+
+  // used to deserialize the user
+  passport.deserializeUser(function (id, done) {
+    userRep.getById({_id: id}, function (err, user) {
+      done(err, user)
+    })
+  })
+
+  passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  },
+  (email, password, done) => {
+    userRep.get({ email }, (err, user) => {
+      if (err) { return done(err) }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect email.' })
+      }
+      if (!user.password === password) {
+        return done(null, false, { message: 'Incorrect password.' })
+      }
+      return done(null, user)
+    })
+  }
+  ))
+}
