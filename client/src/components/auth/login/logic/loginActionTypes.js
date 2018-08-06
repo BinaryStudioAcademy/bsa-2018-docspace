478e839ba5d17ff1b0a3894d6910ee59f8570151
jimbomaniak <jimboman54321@gmail.com>
2018-08-06 08:59:05 +0300

    add new  action types

--- client/src/components/auth/login/logic/loginActionTypes.js
+++ client/src/components/auth/login/logic/loginActionTypes.js
@@ -1,3 +1,5 @@
-export const LOGIN = 'LOGIN'
+export const LOGIN_REQUESTING = 'LOGIN_REQUESTING'
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
-export const LOGIN_FAILED = 'LOGIN_FAILED'
+export const LOGIN_ERROR = 'LOGIN_ERROR'
+export const LOGIN_EXISTING = 'LOGIN_EXISTING'
+export const LOGOUT = 'LOGOUT'
