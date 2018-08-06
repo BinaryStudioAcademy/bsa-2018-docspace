f429e247405749db7a508b285d91748afdffa0f1
Daryna Gavrylenko <deakozyulka@gmail.com>
2018-08-06 20:04:13 +0300

    feature/space-page: add blog component

--- client/src/components/blog/index.js
+++ client/src/components/blog/index.js
@@ -5,7 +5,13 @@ import './blog.css'
 class Blog extends Component {
   render () {
     return (
-      <div>It's blog!</div>
+      <div className='blog-container'>
+        <div className='empty-blog-message'>
+          <div className='empty-blog-message__title'>Got something to share?</div>
+          <div className='empty-blog-message__text'>Blog posts are a great way to share announcements, status reports or other important news with your team.</div>
+          <button className='empty-blog-message__button'>Create Blog Post</button>
+        </div>
+      </div>
     )
   }
 }
