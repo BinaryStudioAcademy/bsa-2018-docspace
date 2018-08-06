ace68d8f32f8a9270c3e3701656902ae20f6a39d
jimbomaniak <jimboman54321@gmail.com>
2018-08-06 08:57:17 +0300

    add login route

--- server/routes/api/routes.js
+++ server/routes/api/routes.js
@@ -1,9 +1,11 @@
 const space = require('./space')
 const user = require('./user')
 const signup = require('./auth/signup')
+const login = require('./auth/login')
 
 module.exports = (app) => {
   app.use('/api/signup', signup)
+  app.use('/api/login', login)
   app.use('/api/user', user)
   app.use('/api/spaces', space)
 }
