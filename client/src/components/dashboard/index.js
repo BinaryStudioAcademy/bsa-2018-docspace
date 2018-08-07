eb1e64844bcdfd649448d9e7f68201763f3d9cea
Khrystyna Roboretska <christina.roboretska@gmail.com>
2018-08-07 17:36:18 +0300

    Added responsive sidebar

--- client/src/components/dashboard/index.js
+++ client/src/components/dashboard/index.js
@@ -1,70 +1,54 @@
 import React, {Component} from 'react'
-import {Route, Switch} from 'react-router-dom'
-import SplitPane from 'react-split-pane'
-import FullSidebar from '../sidebar/fullSidebar'
-import MinSidebar from '../sidebar/minSidebar'
+import Dashboard from './sidebar'
+import FullSidebar from './sidebar/fullSidebar'
+import PropTypes from 'prop-types'
 
-export default class Dashboard extends Component {
+class DashboardWrapper extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      isOpened: true,
-      defaultSize: 300,
-      maxSize: 600,
-      minSize: 215
+      showLabels: true,
+      showIcons: true
     }
-    this.closeLabeledWindow = this.closeLabeledWindow.bind(this)
+    this.changeLabels = this.changeLabels.bind(this)
   }
-
-  closeLabeledWindow () {
-    if (this.state.isOpened) {
+  changeLabels (size) {
+    if (size <= 215) {
       this.setState({
-        isOpened: !this.state.isOpened,
-        defaultSize: 70,
-        maxSize: 70,
-        minSize: 70
-      })
-    } else {
-      this.setState({
-        isOpened: !this.state.isOpened,
-        defaultSize: 300,
-        maxSize: 600,
-        minSize: 215
+        showLabels: false
       })
     }
+    if (size > 215) {
+      this.setState({
+        showLabels: true
+      })
+    }
+    if (size <= 130) {
+      this.setState({
+        showIcons: false
+      })
+    }
+    if (size > 130) {
+      this.setState({
+        showIcons: true
+      })
+    }
   }
-
   render () {
-    console.log(this.state)
+    const showIconForTabs = true
     return (
-      <React.Fragment>
-        <SplitPane split='vertical' minSize={this.state.minSize} defaultSize={this.state.defaultSize}
-          maxSize={this.state.maxSize}
-          onChange={size => { if (size < 70) { this.setState({isOpened: false}) } else { this.setState({thisOpened: true}) } }}>
-          <div className='sidebar-container'>
-            {this.state.isOpened &&
-            <div className='sidebar-wrapper'>
-              <FullSidebar
-                sidebarAction={this.closeLabeledWindow} />
-            </div>}
-            {!this.state.isOpened &&
-            <div className='minimize-dashboard-wrapper'>
-              <MinSidebar sidebarAction={this.closeLabeledWindow} color='#8CC152' /></div>
-            }
-          </div>
-          <div>
-            <Switch>
-              //Add routes with components here
-              <Route path='/' />
-              <Route path='/activity' />
-              <Route path='/works' />
-              <Route path='/spaces' />
-              <Route path='/people' />
-              <Route path='/settings' />
-            </Switch>
-          </div>
-        </SplitPane>
-      </React.Fragment>
+      <div className='app__root'>
+        <Dashboard changeLable={this.changeLabels} colorSchema={'grey-schema'}
+          rightSidebar={<FullSidebar showLabels={this.state.showLabels} showIcons={this.state.showIcons} />}
+          tabs={<FullSidebar showLabels={this.state.showLabels} showIcons={showIconForTabs} />} />
+      </div>
     )
   }
+}
+
+export default DashboardWrapper
+
+Dashboard.propTypes = {
+  changeLabels: PropTypes.func,
+  showIcons: PropTypes.func
 }
--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import registerServiceWorker from './registerServiceWorker'
 import { Provider } from 'react-redux'
 import { store, sagaMiddleware } from './commonLogic/store'
 import rootSaga from './commonLogic/rootSaga'
-import history from 'src/commonLogic/history'
+import history from './commonLogic/history'
 import {ConnectedRouter} from 'connected-react-router'
 
 sagaMiddleware.run(rootSaga)
