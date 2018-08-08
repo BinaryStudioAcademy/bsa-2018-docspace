71e14d2935053f166907a90381b49f64bd92051e
Daryna Gavrylenko <deakozyulka@gmail.com>
2018-08-08 22:38:58 +0300

    feature/merge-components: move SpaceSetting

--- client/src/components/common/app/index.js
+++ client/src/components/common/app/index.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
 import './app.css'
 import DashboardActivity from '../../dashboard/dashboardActivity'
-import SpaceSettings from 'src/components/spaceSettings'
 
 class App extends Component {
   render () {
     return (
       <div className='app__root'>
         <DashboardActivity />
-        <SpaceSettings />
       </div>
     )
   }
--- client/src/components/space/spaceSettings/index.js
+++ client/src/components/space/spaceSettings/index.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react'
-
+import SpaceOverviewTab from './overview'
+import SpaceSettingsTab from './settings'
+import NavBar from './navBar'
 import './spaceSettings.css'
 
-class SpaceSettings extends Component {
+// will be connected to store. Fetch for space with this name in didMount
+export default class SpaceSettings extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      activeTab: 'overview',
+      renderByActiveTab: {
+        'overview': this.renderOverviewTab,
+        'settings': this.renderSettingsTab
+      }
+    }
+  }
+
+  renderOverviewTab () {
+    return <SpaceOverviewTab />
+  }
+
+  renderSettingsTab () {
+    return <SpaceSettingsTab />
+  }
+
+  handleNavLinkCLick = (tabName) => {
+    this.setState({
+      activeTab: tabName
+    })
+  }
+
   render () {
+    const {renderByActiveTab, activeTab} = this.state
     return (
-      <div>It's space settings!</div>
+      <div className='space-settings-page'>
+        <h1> Space Settings </h1>
+        <NavBar
+          handleNavLinkCLick={this.handleNavLinkCLick}
+          allTabsName={Object.keys(renderByActiveTab)}
+          activeTabName={activeTab}
+        />
+        { renderByActiveTab[activeTab]() }
+      </div>
     )
   }
 }
-
-export default SpaceSettings
