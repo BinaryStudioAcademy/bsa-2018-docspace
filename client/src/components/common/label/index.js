c4fda3d1901b32957522f79d68a2b7f121bc43c5
Alex Chernenko <aleksey.chernenko@gmail.com>
2018-08-10 22:57:27 +0300

    added posibility to hide closebutton

--- client/src/components/common/label/index.js
+++ client/src/components/common/label/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import './label.css'
-
+// To hide close button use this.props.closeButtonHide
 class Label extends Component {
     state = {
       hover: ''
@@ -17,14 +17,17 @@ class Label extends Component {
 
     render () {
       return (
-        <div className={'label' + this.state.hover}
+        <div className={'labelComponent' + this.state.hover}
           onClick={this.props.onClick}
           name={this.props.name}
         >
-          <a className={'label__text'}>
+          <a className={'labelComponent__text'}>
             {this.props.content}
           </a>
-          <button onClick={this.props.onDelete} onMouseOut={() => this.mouseOut()} onMouseOver={() => this.mouseOver()} className={'label__button'}>
+          <button onClick={this.props.onDelete}
+            onMouseOut={() => this.mouseOut()}
+            onMouseOver={() => this.mouseOver()}
+            className={'labelComponent__button' + (this.props.closeButtonHide ? ' closeButtonHide' : '')}>
             <i className={'fas fa-times'} />
           </button>
         </div>
@@ -36,6 +39,7 @@ Label.propTypes = {
   onClick: PropTypes.func,
   onDelete: PropTypes.func,
   name: PropTypes.string,
-  content: PropTypes.string
+  content: PropTypes.string,
+  closeButtonHide: PropTypes.bool
 }
 export default Label
