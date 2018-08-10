d12183574e44044c11dd741d1b1bbe6da5c58993
Khrystyna Roboretska <christina.roboretska@gmail.com>
2018-08-10 21:38:42 +0300

    Added  CRUD comment API

--- server/routes/api/comment.js
+++ server/routes/api/comment.js
@@ -3,10 +3,6 @@ const commentService = require('../../services/commentService')
 
 router.get('/', commentService.findAll)
 
-router.get('/history', commentService.getHistory)
-
-router.get('/comments', commentService.getComments)
-
 router.get('/:id', commentService.findOne)
 
 router.post('/', commentService.add)
@@ -14,7 +10,5 @@ router.post('/', commentService.add)
 router.put('/:id', commentService.findOneAndUpdate)
 
 router.delete('/:id', commentService.findOneAndDelete)
-
-router.put('/:id/delete', commentService.deleteButSaveInHistory)
 
 module.exports = router
