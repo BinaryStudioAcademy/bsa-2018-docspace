dd74c83d8c2799311492dea4f19b5f2eea64335e
Alex Chernenko <aleksey.chernenko@gmail.com>
2018-08-13 14:31:30 +0300

    merge fixes

---
+++ client/src/components/space/spaceSettings/settings/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class SpaceSettingsTab extends Component {
+  render () {
+    return (
+      <div>
+        settings component
+      </div>
+    )
+  }
+}
