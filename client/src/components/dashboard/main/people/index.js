20a742eb206821d496fc3794c83df99294dd3654
Alex Chernenko <aleksey.chernenko@gmail.com>
2018-08-14 14:05:21 +0300

    fixed error in people section

--- client/src/components/dashboard/main/people/index.js
+++ client/src/components/dashboard/main/people/index.js
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './people.css'
 import SpacesSideBar from '../../spacesSidebar'
 const sideBarTabs = ['All People', 'People with Personal Spaces']
 
-const People = () => (
-  <div className={'dashboard__container'} >
-    <div className={'dashboard__content__spaces'}>
-      <div className={'people__header'}>
-        <h2>People Directory</h2>
+class People extends Component {
+  state = {
+    active: ''
+  }
+  updateData = (tab) => {
+    this.setState({ active: tab })
+  }
+  render () {
+    return (
+      <div className={'dashboard__container'} >
+        <div className={'dashboard__content__spaces'}>
+          <div className={'people__header'}>
+            <h2>People Directory</h2>
+          </div>
+          <div className={'spaces__body'}>
+            <div className={'spaces__sidebar'}><SpacesSideBar menuTabs={sideBarTabs} updateData={this.updateData} /></div>
+            <div />
+          </div>
+        </div>
       </div>
-      <div className={'spaces__body'}>
-        <div className={'spaces__sidebar'}><SpacesSideBar menuTabs={sideBarTabs} /></div>
-        <div />
-      </div>
-    </div>
-  </div>
-)
-
+    )
+  }
+}
 export default People
