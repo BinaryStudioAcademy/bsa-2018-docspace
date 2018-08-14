4aa590fb8666e10b1bacc0ede9b5bd7154c7b304
Maksym Koldun <koldun.maks@gmail.com>
2018-08-14 11:56:03 +0300

    fix warnings

--- client/src/commonLogic/rootSaga.js
+++ client/src/commonLogic/rootSaga.js
@@ -1,17 +1,17 @@
 
-import { fork } from 'redux-saga/effects'
+import { fork, all } from 'redux-saga/effects'
 import signupWatcher from 'src/components/auth/signup/logic/signupSaga'
 import loginWatcher from 'src/components/auth/login/logic/loginSaga'
 import spaceSaga from 'src/components/space/spaceContainer/logic/spaceSaga'
 import userSaga from 'src/containers/user/logic/userSaga'
 
 function * rootSaga () {
-  yield [
+  yield all([
     fork(spaceSaga),
     fork(userSaga),
     signupWatcher(),
     loginWatcher()
-  ]
+  ])
 }
 
 export default rootSaga
