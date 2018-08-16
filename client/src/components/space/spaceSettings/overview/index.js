5770a81affa309786cf742670b75a800a566a366
Danil Davydov <Linadakill@gmail.com>
2018-08-16 19:25:34 +0300

    fix eslint error

--- client/src/components/space/spaceSettings/overview/index.js
+++ client/src/components/space/spaceSettings/overview/index.js
@@ -1,15 +1,11 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import { translate } from 'react-i18next'
-import { deleteSpaceRequest } from 'src/components/space/spaceContainer/logic/spaceActions'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
 import SpaceDetails from './spaceDetails'
 import EditSpaceDetailsForm from './editSpaceDetailsForm'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { deleteSpaceRequest } from 'src/components/space/spaceContainer/logic/spaceActions'
-
 
 import './spaceOverviewTab.css'
 
@@ -21,13 +17,13 @@ class SpaceOverviewTab extends Component {
       showDeleteSpaceModal: false // for the future
     }
   }
-  
+
   handleDeleteSpace = () => {
     this.props.actions.deleteSpaceRequest(this.props.space._id)
   }
 
   handleDeleteSpace = () => {
-    this.props.actions.deleteSpaceRequest(this.props.space._id);
+    this.props.actions.deleteSpaceRequest(this.props.space._id)
   }
 
   goToEditDetails =() => {
@@ -89,9 +85,8 @@ function mapDispatchToProps (dispatch) {
 SpaceOverviewTab.propTypes = {
   t: PropTypes.func.isRequired,
   updateSpace: PropTypes.func.isRequired,
-  space: PropTypes.object.isRequired
+  space: PropTypes.object.isRequired,
+  actions: PropTypes.object
 }
 
 export default translate('translations')(connect(null, mapDispatchToProps)(SpaceOverviewTab))
-
-
