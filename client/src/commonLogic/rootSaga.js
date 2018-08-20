e5962b899119af7adf88e93e9f35cd02037945d0
jimbomaniak <jimboman54321@gmail.com>
2018-08-20 21:47:04 +0300

    fix import

--- client/src/commonLogic/rootSaga.js
+++ client/src/commonLogic/rootSaga.js
@@ -7,7 +7,7 @@ import userSaga from 'src/components/containers/user/logic/userSaga'
 import verificationWatcher from 'src/components/auth/verification/logic/verificationSaga'
 import pageSaga from 'src/components/page/logic/pageSaga'
 import commentSaga from 'src/components/page/commentsLogic/commentsSaga'
-import historyWatcher from 'src/commonLogic/historySaga'
+import historyWatcher from 'src/commonLogic/historySaga/historySaga'
 
 function * rootSaga () {
   yield all([
