9f9fc0961b6669133f068cd1924c1eec70502a26
DolynskiyEduard <dolynskiy.eduard@gmail.com>
2018-08-21 16:46:27 +0300

    deploy: change travis config

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -3,7 +3,7 @@
 set -e
 
 docker build -t gcr.io/${PROJECT_ID}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT .
-echo ${PROJECT_ID}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
+
 echo $GCLOUD_SERVICE_KEY_STG | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
@@ -12,7 +12,7 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME
 
-gcloud docker push gcr.io/${PROJECT_ID}/${DOCKER_IMAGE_NAME}
+gcloud docker -- push gcr.io/${PROJECT_ID}/${DOCKER_IMAGE_NAME}
 
 yes | gcloud beta container images add-tag gcr.io/${PROJECT_ID}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_ID}/${DOCKER_IMAGE_NAME}:latest
 
