3b746aaad3a7574a376ad4d7267043c47a761a0f
DolynskiyEduard <dolynskiy.eduard@gmail.com>
2018-08-21 17:24:33 +0300

    deploy: add docker configure to the gcloud

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -12,6 +12,8 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME
 
+gcloud beta auth configure-docker
+
 gcloud docker -- push gcr.io/${PROJECT_ID}/${DOCKER_IMAGE_NAME}
 
 yes | gcloud beta container images add-tag gcr.io/${PROJECT_ID}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_ID}/${DOCKER_IMAGE_NAME}:latest
