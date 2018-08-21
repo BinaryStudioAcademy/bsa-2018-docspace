d5663ab7bd41e290bf3e18d71706f761f9dccffd
DolynskiyEduard <dolynskiy.eduard@gmail.com>
2018-08-21 13:43:30 +0300

    deploy: change travis config

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-docker build -t gcr.io/$PROJECT_ID/$DOCKER_IMAGE_NAME:$TRAVIS_COMMIT .
-
+docker build -t gcr.io/${PROJECT_ID}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT .
+echo ${PROJECT_ID}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
 echo $GCLOUD_SERVICE_KEY_STG | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
@@ -12,14 +12,14 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME
 
-gcloud docker push gcr.io/$PROJECT_ID/$DOCKER_IMAGE_NAME
+gcloud docker push gcr.io/${PROJECT_ID}/${DOCKER_IMAGE_NAME}
 
-yes | gcloud beta container images add-tag gcr.io/$PROJECT_ID/$DOCKER_IMAGE_NAME:$TRAVIS_COMMIT gcr.io/$PROJECT_ID/$DOCKER_IMAGE_NAME:latest
+yes | gcloud beta container images add-tag gcr.io/${PROJECT_ID}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_ID}/${DOCKER_IMAGE_NAME}:latest
 
 kubectl config view
 kubectl config current-context
 
-kubectl set image deployment/$KUBE_DEPLOYMENT_NAME $KUBE_DEPLOYMENT_CONTAINER_NAME=gcr.io/$PROJECT_ID/$DOCKER_IMAGE_NAME:$TRAVIS_COMMIT
+kubectl set image deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_CONTAINER_NAME}=gcr.io/${PROJECT_ID}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
 
 # sleep 30
 # npm run e2e_test
