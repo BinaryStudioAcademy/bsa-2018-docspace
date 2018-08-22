2249441ecb3afad13c20296443fe713115206273
DolynskiyEduard <dolynskiy.eduard@gmail.com>
2018-08-22 19:13:20 +0300

    file-upload: add helper for perform files upload/download to/from AWS

---
+++ server/fileUploadHelper.js
@@ -0,0 +1,27 @@
+const AWS = require('aws-sdk');
+const fs = require('fs');
+//const multerS3 = require('multer-s3'
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+const bucketName = 'docspace';
+const data = {
+  "some key": "some value"
+};
+
+module.exports = {
+  uploadFile: () => {
+    const params = {
+      Bucket: bucketName,
+      Key: 'contacts.csv',
+      Body: JSON.stringify(data, null, 2)
+    };
+
+    s3.upload(params, function(s3Err, data) {
+      if (s3Err) throw s3Err
+      console.log(`File uploaded successfully at ${data.Location}`)
+    });
+  },
+  downloadFile: () => {}
+}
