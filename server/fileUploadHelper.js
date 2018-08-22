f5ddc1d6912dd4761f6d62fef2cf1cfd3ae0ef6e
Eduard Dolynskyi <eduard.dolinskyi@binary-studio.com>
2018-08-22 22:26:13 +0300

    file-upload: .env file, add .env to gitignore file and add module for set env vars from file to system

--- server/fileUploadHelper.js
+++ server/fileUploadHelper.js
@@ -1,27 +1,28 @@
-const AWS = require('aws-sdk');
-const fs = require('fs');
-//const multerS3 = require('multer-s3'
+const AWS = require('aws-sdk')
+// const fs = require('fs');
+// const multerS3 = require('multer-s3'
+require('dotenv').config()
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-});
-const bucketName = 'docspace';
+})
+const bucketName = 'docspace'
 const data = {
-  "some key": "some value"
-};
+  'some key': 'some value'
+}
 
 module.exports = {
   uploadFile: () => {
     const params = {
       Bucket: bucketName,
-      Key: 'contacts.csv',
+      Key: 'simple.file',
       Body: JSON.stringify(data, null, 2)
-    };
+    }
 
-    s3.upload(params, function(s3Err, data) {
+    s3.upload(params, function (s3Err, data) {
       if (s3Err) throw s3Err
       console.log(`File uploaded successfully at ${data.Location}`)
-    });
+    })
   },
   downloadFile: () => {}
 }
