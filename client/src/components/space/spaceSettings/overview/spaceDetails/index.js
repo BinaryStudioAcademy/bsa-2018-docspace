bad5527566fff9c5047a68f8b707ec1d6de6786f
jimbomaniak <jimboman54321@gmail.com>
2018-08-22 12:16:43 +0300

    remove edit link, add margin

--- client/src/components/space/spaceSettings/overview/spaceDetails/index.js
+++ client/src/components/space/spaceSettings/overview/spaceDetails/index.js
@@ -55,7 +55,7 @@ class SpaceDetails extends Component {
                   <Link key={index} to='#' className='space-category'>
                     {category.name}
                   </Link>)
-                : `(${t('None')})` } [<Link to={this.props.pathToCategories}>Edit</Link>]
+                : `(${t('None')})` }
             </td>
           </tr>
           <tr>
@@ -76,7 +76,6 @@ class SpaceDetails extends Component {
 
 SpaceDetails.propTypes = {
   t: PropTypes.func.isRequired,
-  pathToCategories: PropTypes.string.isRequired,
   space: PropTypes.shape
 }
 
