d283d6d9a2f818d0ae06cd9697e59543fc032ff7
Danil Davydov <Linadakill@gmail.com>
2018-08-23 13:34:32 +0300

    fix

--- client/src/components/dashboard/main/activity/allUpdatesTab/index.js
+++ client/src/components/dashboard/main/activity/allUpdatesTab/index.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 const getRightProps = (content) => {
-  if (!content.spaceId) {
+  if (!content.spaceId || !content.pageId) {
     return null
   }
   switch (content.action) {
