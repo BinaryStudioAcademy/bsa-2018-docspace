813194c708284be8e8b4b5e2cb777b1d047771d0
Maksym Koldun <koldun.maks@gmail.com>
2018-08-23 11:15:55 +0300

    filtering and changed email to real

--- client/src/components/dashboard/input/index.js
+++ client/src/components/dashboard/input/index.js
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types'
 class Input extends Component {
   render () {
     return (
-      <input className={this.props.className + ' inputDashboard'} placeholder={this.props.placeholder} autoComplete='off' type='text' />
-
+      <input className={this.props.className + ' inputDashboard'} onChange={this.props.onChange} placeholder={this.props.placeholder} autoComplete='off' type='text' />
     )
   }
 }
 Input.propTypes = {
   className: PropTypes.string,
-  placeholder: PropTypes.string
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func
 }
 export default Input
