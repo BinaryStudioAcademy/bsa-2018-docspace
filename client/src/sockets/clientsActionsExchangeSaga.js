612c7028e0d87b7aecf6dda39a458d0ae689a770
Danil Davydov <Linadakill@gmail.com>
2018-08-26 19:33:32 +0300

    delete console.log

--- client/src/sockets/clientsActionsExchangeSaga.js
+++ client/src/sockets/clientsActionsExchangeSaga.js
@@ -3,8 +3,6 @@ import { takeEvery } from 'redux-saga/effects'
 import socket from './index.js'
 
 function shareReduxActions (action) {
-  console.log(' SHARE ACTION IN SAGA')
-  console.log(action)
   action.type = `${action.type}(EXTERNAL)`
   socket.emit('share redux action', action)
 }
