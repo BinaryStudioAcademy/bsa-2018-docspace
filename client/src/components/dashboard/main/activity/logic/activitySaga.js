9844cbbeb2047b78a508cafc457aee568dc0be91
Oleg Kupriianov <jimboman54321@gmail.com>
2018-08-27 21:37:12 +0300

    add single user  logic

--- client/src/components/dashboard/main/activity/logic/activitySaga.js
+++ client/src/components/dashboard/main/activity/logic/activitySaga.js
@@ -2,15 +2,25 @@ import { takeEvery, call, put } from 'redux-saga/effects'
 import HistoryService from 'src/services/historyService'
 import * as actionTypes from './activityActionTypes'
 
-function * activityFlow (action) {
+function * allUpdatesFlow (action) {
   try {
-    let allUserHistory = yield call(HistoryService.getAllUserHistory, action.payload.userId)
+    let allUserHistory = yield call(HistoryService.getAllUserHistory)
     yield put({ type: actionTypes.GET_ALL_UPDATES_SUCCESS, allUserHistory })
   } catch (error) {
     yield put({ type: actionTypes.GET_ALL_UPDATES_ERROR, error })
   }
 }
 
+function * currentUserUpdatesFlow (action) {
+  try {
+    let currentUserHistory = yield call(HistoryService.getUserHistory, action.payload.userId)
+    yield put({ type: actionTypes.GET_CURRENT_USER_UPDATES_SUCCESS, currentUserHistory })
+  } catch (error) {
+    yield put({ type: actionTypes.GET_CURRENT_USER_UPDATES_ERROR, error })
+  }
+}
+
 export default function * selectionsSaga () {
-  yield takeEvery(actionTypes.GET_ALL_UPDATES_REQUEST, activityFlow)
+  yield takeEvery(actionTypes.GET_ALL_UPDATES_REQUEST, allUpdatesFlow)
+  yield takeEvery(actionTypes.GET_CURRENT_USER_UPDATES_REQUEST, currentUserUpdatesFlow)
 }
