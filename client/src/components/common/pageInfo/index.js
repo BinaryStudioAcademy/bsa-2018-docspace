2be115982d29fc88e13415e2103ac282c5e6ab1b
Oleg Kupriianov <jimboman54321@gmail.com>
2018-08-30 20:00:02 +0300

    add link to avatar and name

--- client/src/components/common/pageInfo/index.js
+++ client/src/components/common/pageInfo/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import './pageInfo.css'
 
 const PageInfo = ({ avatar, firstName, lastName, date, login }) => {
   return (
     <div className='page-info-container'>
-      <NavLink className='page-info-image' to={`user/${login}`}>
+      <Link className='page-info-image' to={`users/${login}`}>
         <img src={avatar} alt='UserAvatar' />
-      </NavLink>
+      </Link>
       <div className='page-info'>
-        <NavLink className='page-info-author' to={`user/${login}`}>{firstName + ' ' + lastName}</NavLink>
+        <Link className='page-info-author' to={`users/${login}`}>{firstName + ' ' + lastName}</Link>
         <div className='page-info-time'>{date}</div>
       </div>
     </div>
