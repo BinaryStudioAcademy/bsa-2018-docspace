c4fca2c7f92e0c31229f7d58010f8b4db17055d3
Roman Sahan <roman.sahan@binary-studio.com>
2018-08-31 11:35:14 +0300

    change port on production

--- client/src/sockets/createClientSocket.js
+++ client/src/sockets/createClientSocket.js
@@ -1,8 +1,8 @@
 import io from 'socket.io-client'
 
 export default (socketEndpointPort) => {
-  const currentLocation = new URL(window.location.href)
-  const socketEndpoint = `${currentLocation.protocol}//${currentLocation.hostname}:${socketEndpointPort}`
+  // const currentLocation = new URL(window.location.href)
+  // const socketEndpoint = `${currentLocation.protocol}//${currentLocation.hostname}:${socketEndpointPort}`
 
-  return io(socketEndpoint)
+  return io()
 }
