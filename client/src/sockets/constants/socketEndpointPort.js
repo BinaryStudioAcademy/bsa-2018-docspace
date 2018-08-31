a7845cf88fd85e8876366deb72604755e48a0b90
Roman Sahan <roman.sahan@binary-studio.com>
2018-08-31 11:18:34 +0300

    change port on production

--- client/src/sockets/constants/socketEndpointPort.js
+++ client/src/sockets/constants/socketEndpointPort.js
@@ -2,7 +2,8 @@
 // TODO: find a way to pass server port from server... ? Like from .env file...
 // Think about it
 
-const serverPort = (process.env.NODE_ENV === 'production')
-  ? process.env.PORT
-  : 3001
+// const serverPort = (process.env.NODE_ENV === 'production')
+//  ? process.env.PORT
+// : 3001
+const serverPort = 8080
 export default serverPort
