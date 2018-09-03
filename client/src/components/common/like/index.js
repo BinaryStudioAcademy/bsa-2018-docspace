0c2b936d488c73f54a3a67400adebc81b48e802a
Khrystyna Roboretska <christina.roboretska@gmail.com>
2018-09-03 19:56:24 +0300

    Implemented feedback

--- client/src/components/common/like/index.js
+++ client/src/components/common/like/index.js
@@ -34,7 +34,7 @@ class Like extends Component {
 
   sortLikes () {
     this.props.likes.sort((a) => {
-      return a.id === this.props.user ? 1 : -1
+      return a.id === this.props.user ? -1 : 1
     })
   }
 
@@ -62,7 +62,7 @@ class Like extends Component {
         message += i === count - 1 ? name + '' : name + ', '
       }
       message += likes.length > 3 ? t('and_0_ other_people', { count: likes.length - 3 }) : ''
-      message += ' ' + t('already_like_it')
+      message += likes.length === 1 && !this.state.isCurrentUserLike ? ' ' + t('already_likes_it') : ' ' + t('already_like_it')
     }
     return message
   }
